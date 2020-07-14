70aa46d7238e015fbdb5f1c694c9a06c0410b5d1
Eric MULLER <eric.pierre.muller@gmail.com>
2020-07-14 13:33:16 +0200

    Add heroku config

---
+++ src/environments/environment.heroku.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  application: 'QCM ',
+  production: true,
+  KEYCLOAK: true,
+  KEYCLOAK_URL: 'https://keycloak.webmarks.net/auth',
+  KEYCLOAK_REALM: 'qcm',
+  KEYCLOAK_CLIENTID: 'qcm-web-dev',
+  PAGE_SIZE: 100,
+  QCM_REST_API_HOST: 'https://qcm-rest-api.herokuapp.com'
+};
